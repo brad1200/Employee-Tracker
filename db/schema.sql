ff3267a28ee6ffdda3ac07415f8db039483b9432
Brad <bycarlson23@gmail.com>
2022-09-05 14:34:57 -0500

    update on sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,43 +1,34 @@
+
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
 USE employee_db;
+
 CREATE TABLE department (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(30) NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL, 
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dep_ind(department_id),
+    CONSTRAINT fk_department
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) ON DELETE CASCADE
+);
 
-
-
-
--- DROP DATABASE IF EXISTS employee_db;
--- CREATE DATABASE employee_db;
-
--- USE employee_db;
-
--- CREATE TABLE department (
---     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(30) UNIQUE NOT NULL,
--- );
-
--- CREATE TABLE role (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     title VARCHAR(30) NOT NULL, 
---     salary DECIMAL,
---     department_id INT,
---     FOREIGN KEY (department_id)
---     REFERENCES department(id) ON DELETE SET NULL
--- );
-
--- CREATE TABLE employee (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(30) NOT NULL,
---     last_name VARCHAR(30) NOT NULL,
---     role_id INT,
---     manager_id INT,
---     FOREIGN KEY (role_id)
---     REFERENCES role(id),
---     FOREIGN KEY (manager_id)
---     REFERENCES  employee(id) ON DELETE SET NULL
--- );
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES  employee(id) ON DELETE SET NULL
+);
 
