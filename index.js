6e1190c29377063c598a39e3033daef22c0dce80
Brad <bycarlson23@gmail.com>
2022-09-05 13:00:30 -0500

    setting up intial promt

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 require('dotenv').config()
 const dbConfig = require('./config/dbConfig');
 
@@ -9,4 +11,45 @@ async function main() {
     console.info(chalk.blue('connection to database'));
     const dbConnection = await dbConfig();
     console.log(dbConnection);
-}
+};
+
+function startPromt()
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            choices: [
+                {
+                    name: 'View all departments',
+                    value: 'VIEW_ALL_DEPARTMENTS'
+                },
+                {
+                    name: 'View all roles',
+                    value: 'VIEW_ALL_ROLES'
+                },
+                {
+                    name: 'View all employees',
+                    value: 'VIEW_ALL_EMPLOYEES'
+                },
+                {
+                    name: 'Add a department',
+                    value: 'ADD_DEPARTMENT'
+                },
+                {
+                    name: 'Add a role',
+                    value: 'ADD_ROLE'
+                },
+                {
+                    name: 'Add an employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name: 'Update and employee role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
+                }
+            ]
+        }
+    ]);
+
+startPromt();
