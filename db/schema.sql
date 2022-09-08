f93ac24f486931c21ad463c681882d89fc1e204c
Brad <bycarlson23@gmail.com>
2022-09-07 20:59:44 -0500

    got seeds and schema running

--- db/schema.sql
+++ db/schema.sql
@@ -20,13 +20,17 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT UNSIGNED,
-    manager_id INT,
+    role_id INT UNSIGNED NOT NULL,
+    INDEX role_id (role_id),
+    CONSTRAINT fk_role
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager
     FOREIGN KEY (manager_id)
     REFERENCES  employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO department (id, name)
-VALUES (1, 'Salesman'),
-        (2, 'Main building'),
-        (3, 'Out of building');
+INSERT INTO department (name)
+VALUES ('Salesman'),
+        ('Main building'),
+        ('Out of building');
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Lead Salesman', 100000, 1),
-        (2, 'Greeter', 50000, 2),
-        (3, 'Porter', 45000,3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Lead Salesman', 100000, 1),
+        ('Greeter', 50000, 2),
+        ('Porter', 45000,3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Brad', 'Carlson', 1, 1),
-        (2, 'Nick', 'Goner', 2, 4),
-        (3, 'Deion', 'Lucky', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Brad', 'Carlson', 1, NULL),
+        ('Nick', 'Goner', 2, 1),
+        ('Deion', 'Lucky', 3, NULL);
